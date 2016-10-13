6f88a370fcb034f83983854a2525d9b79b8701b4
santiago <smartnix@hotmail.com>
2016-10-13 14:58:49 -0500

    Se realizo validaciones en model user para logitud, email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     has_many :microposts
-    validates :name, presence: true            
-    validates :email, presence: true
+    validates :name, presence: true, length: {maximum: 20}            
+    validates :email, presence: true, length: {maximum: 30}, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
 end
