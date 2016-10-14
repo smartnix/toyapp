cc3465aa934df2c3d8b937d594173c9f3e01eeb1
santiago <smartnix@hotmail.com>
2016-10-14 09:07:36 -0500

    Se adiciono convertir el mail a downcase antes de guardar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
+   
 class User < ApplicationRecord
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    before_save {self.email = email.downcase}
     has_many :microposts
-    validates :name, presence: true, length: {maximum: 20}            
-    validates :email, presence: true, length: {maximum: 30}, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+    validates :name, presence: true, length: {maximum: 20}  
+     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i          
+    validates :email, presence: true, length: {maximum: 30}, format: {with: VALID_EMAIL_REGEX}, 
+               uniqueness: {case_sensitive: false}
 end
