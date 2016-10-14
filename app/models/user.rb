2f515162d10809c7c72b48b7b7a6c5565310aa41
santiago <smartnix@hotmail.com>
2016-10-14 11:29:19 -0500

    Se adiciono gem bcrypt, para las contraseñas y se implemento text area para los password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
      VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i          
     validates :email, presence: true, length: {maximum: 30}, format: {with: VALID_EMAIL_REGEX}, 
                uniqueness: {case_sensitive: false}
+    has_secure_password
+    validates :password_digest, presence: true, length: {minimum:4}
 end
