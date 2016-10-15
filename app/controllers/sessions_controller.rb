be1d684765a0b006805ed2f1a28946d014f4552c
santiago <smartnix@hotmail.com>
2016-10-15 14:16:56 -0500

    Se corrigio el form de login, se comenzo con la implementacion de la sesion

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+
+    else
+      flash.now[:danger] = "Combinacion de email/password invalido!"
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
 end
