d2c3a2790f909b427d6ea7370d51d06cc028ca6a
santiago <smartnix@hotmail.com>
2016-10-16 00:30:31 -0500

    Se implemento la sesion de login, con cookies permamentes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
    
 class User < ApplicationRecord
+    attr_accessor :remember_token
     before_save {self.email = email.downcase}
     has_many :microposts
     validates :name, presence: true, length: {maximum: 20}  
@@ -8,4 +9,30 @@ class User < ApplicationRecord
                uniqueness: {case_sensitive: false}
     has_secure_password
     validates :password_digest, presence: true, length: {minimum:4}
+
+    # Returns the hash digest of the given string.
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST:
+                                                        BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Returns a random token.
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+    # Remembers a user in the database for use in persistent sessions.
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
 end
