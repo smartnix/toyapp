296b2754cddac4475f6071afc3271225079e4406
santiago <smartnix@hotmail.com>
2016-10-15 20:25:10 -0500

    se finalizo las sesion para el login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  include SessionHelper
+  include SessionsHelper
   def hello
     render html: "Hola mundo"
     
