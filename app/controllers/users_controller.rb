05c264e656982f9d0d85105b7c526417f290bcb6
santiago <smartnix@hotmail.com>
2016-10-16 09:40:50 -0500

    se implemento que solo los usurios logeados puedan editar y actualizar para si mismos n para otros usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  before_action :logged_in_user,  only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   # GET /users
   # GET /users.json
   def index
@@ -45,7 +46,8 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        flash[:success] = "Profile updated" 
+        format.html { redirect_to @user }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
@@ -64,6 +66,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
@@ -73,5 +82,11 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -36,4 +36,9 @@ module SessionsHelper
         cookies.delete(:user_id)
         cookies.delete(:remember_token)
     end
+
+    # Returns true if the given user is the current user.
+    def current_user?(user)
+        user == current_user
+    end
 end
